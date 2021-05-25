e2858518821a19be6961c80bcef056dcf0890795
pemudakoding <pemudakoding@gmail.com>
2021-05-25 16:51:23 +0800

    modified: translator just have 2 languange

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ AOS.init();
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({
       pageLanguage: 'en',
-      layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+      layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+      includedLanguages: "en,id"
     }, 'google_translate_element');
   }
